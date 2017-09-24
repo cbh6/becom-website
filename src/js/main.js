95dca30c94cbe490699d02ccadf82e6db4e4f10a
cbh6 <cri.bh6@gmail.com>
2017-09-24 11:44:32 +0200

    init test version finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-//=require ../../lib/PaperKit/js/jquery-3.2.1.min.js"
 //=require ../../lib/PaperKit/js/jquery-ui-1.12.1.custom.min.js
 //=require ../../lib/PaperKit/js/tether.min.js
 //=require ../../lib/PaperKit/js/bootstrap.min.js
@@ -93,4 +92,25 @@ function smoothScroll(target) {
     },
     600
   );
+}
+
+function initContactUsMap(){
+    var myLatlng = new google.maps.LatLng(38.268745,-0.7126194);
+    var mapOptions = {
+      zoom: 14,
+      center: myLatlng,
+      styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
+      scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+    }
+
+    var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"Oficina de Becom"
+    });
+
+    // To add the marker to the map, call setMap();
+    marker.setMap(map);
+
 }
