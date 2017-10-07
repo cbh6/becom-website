206f8c78134dee7d9c62f14cd4db330bd6392408
cbh6 <cri.bh6@gmail.com>
2017-10-07 12:00:27 +0200

    changes. info in each section

--- src/js/main.js
+++ src/js/main.js
@@ -143,8 +143,11 @@ function checkScroll(){
 
     if($(window).scrollTop() > startY){
         $('.navbar').removeClass("transparent");
+        $('#logo').attr('src', 'assets/img/logosinfondonegro.png');
     }else{
         $('.navbar').addClass("transparent");
+        if(!$('#inicio').hasClass('becom-color'))
+          $('#logo').attr('src', 'assets/img/logosinfondoblanco.png');
     }
 }
 
